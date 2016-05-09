71b594d563ca5badf1a1c113f4475a584fe91cf0
Sindormir.net <sindormir@sindormir.net>
2016-05-09 09:14:53 +0200

    Uso de caracteres personalizados

--- tema12.ino
+++ tema12.ino
@@ -2,10 +2,22 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //Pines donde está conectado el LCD
 
+byte sindormir[8] = { 
+  B00000, 
+  B01111, 
+  B10001, 
+  B10101, 
+  B10001, 
+  B10001, 
+  B11111, 
+}; 
+
 void setup() {
   lcd.begin(16, 2); //16 columnas y 2 filas
   lcd.setCursor(0,0);
-  lcd.print("Hola mundo!");
+  lcd.write(byte(0)); 
+  lcd.write(byte(0)); 
+  lcd.print(" Hola mundo!");
   lcd.setCursor(0,1);
   lcd.print("millis: ");
 }
