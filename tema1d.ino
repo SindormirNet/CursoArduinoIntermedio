adf4e42cb1e5a9e561522680015733ca263eef1f
Jorge Gómez <syvic@sindormir.net>
2019-10-20 11:18:30 +0200

    Create tema1d.ino

---
+++ tema1d.ino
@@ -0,0 +1,17 @@
+void setup() {
+  unsigned long mi_variable = 2.7182;
+  Serial.begin(9600);
+  Serial.print("Escribiendo en EEPROM: ");
+  Serial.println(mi_variable);
+  EEPROM.put(POS_LONG, mi_variable);
+}
+
+void loop() {
+  unsigned long mi_variable_recuperada;
+
+  Serial.print("Leyendo de EEPROM: ");
+  EEPROM.get(POS_EEPROM, mi_variable_recuperada);
+  Serial.println(mi_variable_recuperada);
+  while(1); // Paramos aquí el código
+
+}
